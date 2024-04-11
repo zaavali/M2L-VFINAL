e9d59a900aec3130e8399437d8de9f15113fa447
nnarion <116152623+nnarion@users.noreply.github.com>
2024-04-11 15:33:57 +0200

    a

--- BACK/server.js
+++ BACK/server.js
@@ -8,10 +8,7 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:58195', 'http://192.168.1.39:3000'],
-  credentials: true,
-}));
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 app.use('/api/user', userRoute);
 app.use('/api/prod', prodRoute);
