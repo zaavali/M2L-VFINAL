bcf296315ed40e33aced5930d5f6a705da8baae4
nnarion <116152623+nnarion@users.noreply.github.com>
2024-04-11 12:21:57 +0200

    cors

--- BACK/server.js
+++ BACK/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:58195'],
+  origin: ['http://localhost:3000', 'http://localhost:58195', 'http://192.168.1.39:3000'],
   credentials: true,
 }));
 app.use('/uploads', express.static('uploads'));
